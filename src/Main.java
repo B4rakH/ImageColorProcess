6109393c23d8caf25faaaff886f79735d3dc6099
BurakH <burakhasimoglu@ogr.eskisehir.edu.tr>
2025-08-18 00:35:26 +0300

    Revert "Implementing Multi-Threaded color change processing"
    This reverts commit 2462b04c1815cbf68f758d301382014746e7a91a.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-//        ImageProcessManager.startRecolor(4);
-//
-//        System.out.println(ImageProcessManager.getDuration() + " ms");
-
+    public static void main(String[] args) {
+        
         }
 }
