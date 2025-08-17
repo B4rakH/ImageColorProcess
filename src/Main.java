59e1dfbcb0068d195dde3d888ca5ba16107c8330
BurakH <burakhasimoglu@ogr.eskisehir.edu.tr>
2025-08-18 00:38:14 +0300

    Implementing Multi-Threaded color change processing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
+//        ImageProcessManager.startRecolor(4);
+//
+//        System.out.println(ImageProcessManager.getDuration() + " ms");
+
         }
 }
